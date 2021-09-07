e04c9674440978b40e99301446e51d4d25b3788f
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2021-09-07 21:38:13 +0300

    initial commit

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   const rows = document.querySelectorAll('tbody tr');
 
+   function onClick() {
+      const text = document.getElementById('searchField').value;
+
+      for (let row of rows) {
+         if (row.textContent.includes(text)) {
+            row.classList.add('select');
+         } else {
+            row.removeAttribute('class');
+         }
+      }
    }
 }
--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+    const input = document.getElementById('input').value.split('.');
+    const output = document.getElementById('output');
+
+    let i = 0;
+    while (i < input.length) {
+        if (input[i].length < 1) {
+            input.splice(i, 1);
+        } else {
+            i += 1;
+        }
+    } 
+    
+    for (i = 0; i < input.length; i += 3) {
+        let parHTML = document.createElement('p');
+        parHTML.textContent = input.slice(i, i+3).join('.') + '.';
+        output.appendChild(parHTML);
+    }
 }
