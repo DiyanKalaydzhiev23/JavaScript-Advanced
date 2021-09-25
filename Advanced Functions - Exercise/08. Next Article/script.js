c8bd9d01b895c3d7b902dee566d19a7c59a9c557
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2021-09-25 13:33:08 +0300

    initial commit

--- Advanced Functions - Exercise/08. Next Article/script.js
+++ Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,10 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const divEl = document.getElementById('content');
+    return function() {
+        if (articles.length > 0) {
+            const article = document.createElement('article');
+            article.textContent = articles.shift();
+            divEl.appendChild(article);
+        }
+    }
 }
